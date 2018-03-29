181fa7a08c3ba81ddc245970022b6d59dff255ca
Hogeyama <gan13027830@gmail.com>
2018-03-29 15:56:54 +0900

    refactor ToJSON instance

--- src/Neovim/LSP/Protocol/Type/Instance.hs
+++ src/Neovim/LSP/Protocol/Type/Instance.hs
@@ -1,6 +1,8 @@
 
 {-# LANGUAGE ConstraintKinds       #-}
 {-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -10,9 +12,7 @@
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
-{-# LANGUAGE UndecidableInstances  #-} -- 許して
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE UndecidableInstances  #-}
 {-# OPTIONS_GHC -Wall              #-}
 {-# OPTIONS_GHC -Wno-orphans       #-}
 
@@ -25,25 +25,21 @@ module Neovim.LSP.Protocol.Type.Instance
   , FieldJSON
   ) where
 
-import           Control.DeepSeq
 import           Control.Applicative   ((<|>))
+import           Control.DeepSeq
 import           Data.Aeson            hiding (KeyValue, Object)
-import qualified Data.Aeson.Types      as J-- hiding (KeyValue)
-import qualified Data.ByteString.Char8        as B
-import           Data.Constraint
+import qualified Data.Aeson.Types      as J
 import           Data.Extensible       hiding (Nullable)
 import           Data.Functor.Identity (Identity (..))
 import qualified Data.HashMap.Strict   as HM
 import qualified Data.Map              as M
-import           Data.Monoid
 import           Data.Proxy
 import           Data.String           (fromString)
-import           Data.Text             (Text)
-import           GHC.TypeLits          (KnownSymbol, symbolVal)
 import           GHC.Generics          (Generic)
+import           GHC.TypeLits          (KnownSymbol, symbolVal)
 
-import           Neovim                (NvimObject(..))
-import qualified Neovim                as N (Object(..))
+import           Neovim                (NvimObject (..))
+import qualified Neovim                as N (Object (..))
 
 -------------------------------------------------------------------------------
 -- Some data types
@@ -65,7 +61,7 @@ type Nullable = Maybe
 -- >>> BL.putStrLn $ encode $ toJSON recordOpt
 -- {}
 --
--- TODO 説明.
+-- Inversely,
 --
 -- >>> decode "{\"id\":null}" :: Maybe (Record '["id" >: Maybe  Int])
 -- Just (id @= Nothing <: nil)
@@ -113,7 +109,6 @@ infixr 4 :|:
 -- JSON
 -------------------------------------------------------------------------------
 
--- | TODO 説明
 type FieldJSON a = (FieldToJSON a, FieldFromJSON a)
 
 -- From JSON
@@ -129,13 +124,12 @@ instance FromJSON a => FieldFromJSON' 'True (Option a) where
     -- in response to a hover request:
     --   '{"result":{"contents":[{"language":"rust","value":"&str"}],"range":null}}'
     -- However, 'range' cannot be 'null' here in the LSP specification.
-    Just x  -> Some <$> parseJSON x
-    Nothing -> return None
+    Just x    -> Some <$> parseJSON x
+    Nothing   -> return None
 instance FromJSON a => FieldFromJSON' 'False a where
   lookupD _ k v = case HM.lookup (fromString k) v of
     Just x  -> parseJSON x
     Nothing -> fail $ "Missing key: " ++ k
--- UndecidableInstanceが必要になるけど許して
 class    (FieldFromJSON' (IsOptional a) a) => FieldFromJSON a
 instance (FieldFromJSON' (IsOptional a) a) => FieldFromJSON a
 
@@ -157,28 +151,22 @@ instance ToJSON a => FieldToJSON' 'True (Option a) where
   toJSON' _ None     = Nothing
 instance ToJSON a => FieldToJSON' 'False a where
   toJSON' _ = Just . toJSON
-
--- UndecidableInstanceが必要になるけど許して
 class    (FieldToJSON' (IsOptional a) a) => FieldToJSON a
 instance (FieldToJSON' (IsOptional a) a) => FieldToJSON a
+toJSON_ :: forall a. FieldToJSON a => a -> Maybe Value
+toJSON_ = toJSON' (Proxy @(IsOptional a))
 
--- TODO rewrite with hfoldlWithIndexFor
 instance Forall (KeyValue KnownSymbol FieldToJSON) xs => ToJSON (Record xs) where
-  toJSON rec = J.Object $ HM.fromList $ flip appEndo [] $
-    hfoldMap getConst' $ hzipWith zipper
-        (library :: Comp Dict (KeyValue KnownSymbol FieldToJSON) :* xs)
-        rec
-    where zipper :: forall x.
-                    Comp Dict (KeyValue KnownSymbol FieldToJSON) x
-                 -> Field Identity x
-                 -> Const' (Endo [(Text,Value)]) x
-          zipper (Comp Dict) v = case mval of
-              Just val -> Const' $ Endo ((field, val):)
-              Nothing  -> Const' $ Endo id
-            where mval  = toJSON' (Proxy @(IsOptional (AssocValue x))) $
-                            runIdentity $ getField v
-                  field = fromString $ symbolVal $ proxyAssocKey v
-
+  toJSON xs = J.Object $ hfoldlWithIndexFor
+    (Proxy @(KeyValue KnownSymbol FieldToJSON))
+    (\_ m kv ->
+      let key  = fromString $ symbolVal $ proxyAssocKey kv
+          mval  = toJSON_ $ runIdentity $ getField kv
+      in case mval of
+        Nothing  -> m
+        Just val -> HM.insert key val m)
+    HM.empty
+    xs
 
 -------------------------------------------------------------------------------
 -- NvimObject
@@ -192,19 +180,20 @@ instance NvimObject o => FieldNvimObject' 'False o where
 instance NvimObject o => FieldNvimObject' 'True (Option o) where
   toObject' _ None     = Nothing
   toObject' _ (Some x) = Just $ toObject x
-
 class    (FieldNvimObject' (IsOptional a) a) => FieldNvimObject a
 instance (FieldNvimObject' (IsOptional a) a) => FieldNvimObject a
+toObject_ :: forall a. FieldNvimObject a => a -> Maybe N.Object
+toObject_ = toObject' (Proxy @(IsOptional a))
 
 instance (NFData (Record xs), Forall (KeyValue KnownSymbol FieldNvimObject) xs)
           => NvimObject (Record xs) where
   toObject xs = N.ObjectMap $ hfoldlWithIndexFor
     (Proxy @(KeyValue KnownSymbol FieldNvimObject))
-    (\(_ :: Membership xs x) m kv ->
-      let key  = N.ObjectString $ B.pack $ symbolVal $ proxyAssocKey kv
-          mval = toObject' (Proxy @(IsOptional (AssocValue x))) $ runIdentity $ getField kv
+    (\_ m kv ->
+      let key  = N.ObjectString $ fromString $ symbolVal $ proxyAssocKey kv
+          mval = toObject_ $ runIdentity $ getField kv
       in case mval of
-        Nothing -> m
+        Nothing  -> m
         Just val -> M.insert key val m)
     M.empty
     xs
